04c36ab5d8241ef8da0b9fe7052bd931a614f51f
NirabHossain <nirabamth@gmail.com>
2022-05-30 23:09:43 +0600

    admin

--- index.js
+++ index.js
@@ -186,7 +186,12 @@ async function run() {
 
 
 
-
+        app.get('/admin/:email',verifyJWT,async(req, res)=>{
+            const email = req.params.email;
+            const user = await usersCollection.findOne({email: email});
+            const isAdmin = user.role==="admin";
+            res.send({admin: isAdmin});
+        })
 
 
 
